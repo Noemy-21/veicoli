431e227c3bcfe14c0bfbdcb4cce7ce2a33dd64d5
Luigis <meda34877@gmail.com>
2025-02-13 06:47:45 +0100

    Delete Concessionaria.py

--- Concessionaria.py
+++
@@ -1,9 +0,0 @@
-class Concessionaria:
-    def __init__(self, id_veicolo, marca, modello, anno, tipo):
-        self.id_veicolo = id_veicolo
-        self.marca = marca
-        self.modello = modello
-        self.anno = anno
-        self.tipo = tipo
-
-
